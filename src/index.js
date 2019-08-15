bea8ddd24d506c24f7a7e9504279fd57f9188e2b
Katerina Panchenko <buddhagolden@gmail.com>
2019-08-15 23:28:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -157,7 +157,8 @@ function getData() {
 //rendering card
 function renderCards(data) {
     const goodsWrapper = document.querySelector('.goods');
-    data.goods.forEach((good) => { //переберем все товары в массиве goods (взято из базы данных, свойства обьекта)
+    //переберем все товары в массиве goods (взято из базы данных, свойства обьекта)
+    data.goods.forEach((good) => { 
         const card = document.createElement('div'); //создали элемент - див карточки в которой будет верстка для товара
         card.className = 'col-12 col-md-6 col-lg-4 col-xl-3';
         //далее проверяем есть ли у товара скидка (распродажа) и выводим соответствующий стикер на товаре
